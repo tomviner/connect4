de3c5341e80ccef994254f0c5b893f9f40b6188f
Nicholas H.Tollervey <ntoll@ntoll.org>
2011-09-08 21:22:29 +0100

    checks for 2 winning conditions

--- board.py
+++ board.py
@@ -1,4 +1,10 @@
 
+def list_get(lst, index, default=None):
+    try:
+        return lst[index]
+    except IndexError:
+        return default
+
 
 class Board(object):
     def __init__(self):
@@ -11,10 +17,7 @@ class Board(object):
         for row_num in range(self.height):
             line = '|'
             for col in self.board:
-                try:
-                    line += ' ' + col[row_num]
-                except IndexError:
-                    line += ' _'
+                line += ' ' + list_get(col, row_num, '_')
             line += ' |'
             lines.append(line)
         return '\n\n'.join(lines[::-1])
@@ -30,6 +33,40 @@ class Board(object):
             self.board[col_num].append(symbol)
         else:
             raise ValueError("Invalid move!")
+
+    def connects(line, win=4, null_pieces=' '):
+        run = 0
+        token = ' '
+        for piece in line:
+            if piece == token[-1]:
+                run += 1
+                if run == 4 and piece not in null_pieces:
+                    return piece
+            else:
+                run = 1
+                token = piece
+        return None
+
+    def vert_win(self):
+        for col in self.board:
+            winner = connects(col)
+            if winner:
+                return winner
+        return None
+
+    def hori_win(self):
+        for row_num in range(self.height):
+            row = ''.join(list_get(col, row_num, ' ') for col in self.board)
+            winner = connects(row)
+            if winner:
+                return winner
+        return None
+
+    def diag_win(self):
+        return None
+
+    def get_winner(self):
+        return self.vert_win() or self.hori_win() or self.diag_win()
 
     def is_full(self):
         return all(
