440401837819eef78e27067d178e73359ad9b106
Jonathan Hartley <jonathan@rangespan.com>
2011-09-08 20:36:48 +0100

    fix move from gameloop to main

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def main():
     p1 = AI()
     p2 = Human()
     players = [p1, p2]
-    next_player = 0
+    current_player = 0
     while True:
         print board
-        players[next_player].next_move(board)
+        player = players[current_player]
+        board.move(player.get_next_move(board), player.symbol)
+        current_player = (current_player + 1) % 2
 
 if __name__ == '__main__':
     main()
-
-
 
